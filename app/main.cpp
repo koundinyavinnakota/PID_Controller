416e682e66aa3402ee101d1c8b9bdfb87cc1d1be
Aneesh Chodisetty <ch.aneesh1996@gmail.com>
2022-10-03 22:06:23 -0400

    Added calculation and modified tests

--- app/main.cpp
+++ app/main.cpp
@@ -9,9 +9,30 @@
 #include <pid.hpp>
 
 int main() {
+    
+    // Parameters
+    double k_p{0.1};
+    double k_d{0.01};
+    double k_i{0.5};
+    double dt{0.1};
+    double max{50};
+    double min{-50};
+
+    
     // Object initialization for the class PID
-    PID controller_1(0.8, 0.7, 0.6, 2, 8, -8);
+    PID controller_1(k_p, k_d, k_i, dt, max, min);
+    
     // Displaying the computed velocity to the user
-    cout << controller_1.ComputeVelocity(18) << endl;
+    double process_value{20};
+    double set_point{10};
+    double current_difference{0};
+    double total_time{10};
+
+    for (int time_step = 0; time_step < static_cast<int>(total_time / dt); time_step++)
+    {
+        current_difference = controller_1.ComputeVelocity(process_value, set_point);
+        std::cout << "The current velocity is: " << process_value << std::endl;
+        process_value += current_difference;
+    }
     return 0;
 }
